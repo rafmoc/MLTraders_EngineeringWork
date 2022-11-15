f070c4896ba6ca93cbefb50df4165449536e050b
Rafał Olejniczak <olejniczak00@gmail.com>
2022-11-15 16:45:37 +0100

    Un-Hardcoding numberOfTrainingCells for MLLogs

--- Assets/Scripts/TrainingScripts/MLLogs.cs
+++ Assets/Scripts/TrainingScripts/MLLogs.cs
@@ -8,6 +8,9 @@ public class Multi_MLLogs : MonoBehaviour
     // ------
     public static Multi_MLLogs Instance { get; private set; }
 
+    [SerializeField]
+    private int numberOfTrainingCells;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -67,7 +70,7 @@ public class Multi_MLLogs : MonoBehaviour
         mLTrader.tradeBalance = 0;
 
         TimeForWriteToFIleCounter++;
-        if (TimeForWriteToFIleCounter >= 54)
+        if (TimeForWriteToFIleCounter >= numberOfTrainingCells) //Wait for data from all managers
         {
             TimeForWriteToFIleCounter = 0;
             WriteToFile();
